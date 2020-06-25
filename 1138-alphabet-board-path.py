bbcf287fa7925c14c367c6f8e7dc596bed627044
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-06-25 08:57:49 -0700

    completes naive function

--- 1138-alphabet-board-path.py
+++ 1138-alphabet-board-path.py
@@ -4,7 +4,19 @@ def alphabetBoardPath(target):
     for char in target:
         char_val = ord(char)-97
         next_pos = (char_val // 5, char_val % 5)
-        print(next_pos)
+        row_moves = next_pos[0] - cur_pos[0]
+        col_moves = next_pos[1] - cur_pos[1]
+        if col_moves > 0:
+            res += 'R' * col_moves
+        else:
+            res += 'L' * -col_moves
+        if row_moves > 0:
+            res += 'D' * row_moves
+        else:
+            res += 'U' * -row_moves
+        res += '!'
+        cur_pos = next_pos
+    return res
 
-target = 'leet'
-alphabetBoardPath(target)
+target = 'code'
+print(alphabetBoardPath(target))
