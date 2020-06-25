2453a6ad51fd9a07f9a15a334d5dfbf0f4effd91
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-06-25 09:07:06 -0700

    completes alphabet board

--- 1138-alphabet-board-path.py
+++ 1138-alphabet-board-path.py
@@ -6,17 +6,24 @@ def alphabetBoardPath(target):
         next_pos = (char_val // 5, char_val % 5)
         row_moves = next_pos[0] - cur_pos[0]
         col_moves = next_pos[1] - cur_pos[1]
+
         if col_moves > 0:
-            res += 'R' * col_moves
+            col_moves = 'R' * col_moves
         else:
-            res += 'L' * -col_moves
+            col_moves = 'L' * -col_moves
         if row_moves > 0:
-            res += 'D' * row_moves
+            row_moves = 'D' * row_moves
         else:
-            res += 'U' * -row_moves
+            row_moves = 'U' * -row_moves
+
+        if char == 'z':
+            res += col_moves + row_moves
+        else:
+            res += row_moves + col_moves
+            
         res += '!'
         cur_pos = next_pos
     return res
 
-target = 'code'
+target = 'bzv'
 print(alphabetBoardPath(target))
