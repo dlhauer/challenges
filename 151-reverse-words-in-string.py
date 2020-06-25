8fa3fa1213cce114e3e73a87ca917dad23a3a85b
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-06-25 10:07:08 -0700

    completes two pointers solution

--- 151-reverse-words-in-string.py
+++ 151-reverse-words-in-string.py
@@ -1,7 +1,28 @@
-def reverseWords(s):
+def reverseWordsOne(s):
     s = s.split(' ')
     s.reverse()
     return ' '.join([word for word in s if len(word) >= 1])
 
-s = '   hello world! '
-print(reverseWords(s), len(reverseWords(s)))
+def reverseWordsTwo(s):
+    res = ''
+    i = -1
+    while i >= -len(s):
+        while i >= -len(s) and s[i] == ' ':
+            i -= 1
+        j = i
+        while j >= -len(s) and s[j] != ' ':
+            j -= 1
+        if i == -1:
+            word = s[j+1:]
+        else:
+            word = s[j+1:i+1]
+        if word:
+            res += word+' '
+        i = j
+    return res[:-1]
+
+
+
+
+s = '     a    good   example    '
+print(reverseWordsTwo(s), len(reverseWordsTwo(s)))
