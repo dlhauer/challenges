017e644e3942a85cbdf17843caa480a2fd88d3ba
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-06-30 08:39:14 -0700

    completes first bad version;

---
+++ 278-first-bad-version.py
@@ -0,0 +1,15 @@
+def firstBadVersion(n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        lo, hi = 1, n
+        while lo <= hi:
+          mid = (lo+hi)//2
+          if isBadVersion(mid) and (mid == 1 or not isBadVersion(mid-1)):
+            return mid
+          elif not isBadVersion(mid):
+            lo = mid+1
+          else:
+            hi = mid
+        return n
